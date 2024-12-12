0eef8b63b4da1521a95fa6dcefa1130e54227918
LucasBarretto86 <lucas.barretto86@gmail.com>
2024-12-12 15:52:36 -0300

    feat: Implement active storage

--- app/models/course/lesson.rb
+++ app/models/course/lesson.rb
@@ -3,14 +3,32 @@
 class Course::Lesson < ApplicationRecord
   belongs_to :course, inverse_of: :lessons
 
+  has_one_attached :video
+  has_one_attached :thumbnail
+
   validates :title, :description, presence: true
   validates :number, uniqueness: { scope: :course_id }
+
+  validate :video_content_type
+  validate :thumbnail_content_type
 
   before_create :set_lesson_number
 
   private
     def set_lesson_number
       self.number ||= (course.lessons.count + 1)
+    end
+
+    def video_content_type
+      if video.attached? && !video.content_type.in?(%w[video/mp4 video/ogg video/webm])
+        errors.add(:video, "only allows mp4, ogg or webm formats")
+      end
+    end
+
+    def thumbnail_content_type
+      if thumbnail.attached? && !thumbnail.content_type.in?(%w[image/jpeg image/jpg image/png image/webp])
+        errors.add(:thumbnail, "only allows jpeg, jpg, png or webp formats")
+      end
     end
 end
 
